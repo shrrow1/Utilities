09d31b25b2b37d17a3c2007488d0078485723ed8
stu <shrrow1@gmail.com>
2026-06-11 21:09:31 +0100

    Add imapsync

---
+++ ImapSysnc/SyncEmail.sh
@@ -0,0 +1,3 @@
+# https://imapsync.lamiral.info/
+# brew install imapsync
+imapsync  --exclude 'INBOX|Spam|Trash|Deleted Messages|Drafts'   --host1 xxx --user1 'xxx.com' --password1 'xxx' --host2 xxx --user2 'xxx' --password2 'xxx' --ssl1 --ssl2 --syncinternaldates --maxmessages 5
